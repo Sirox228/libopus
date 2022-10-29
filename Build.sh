002b1812552ab4ab333061d32421e453669fd5fb
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-30 01:55:17 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,59 +1,14 @@
-NDK=/home/runner/work/android-ndk-r15c/
-DEST=$(cd "$(dirname "$0")" && pwd)/build/$1
-HOST_ARCH="linux-x86_64"
+NDK=${ANDROID_NDK_HOME}
 
 cd opus
 ./autogen.sh
 # ./configure --host=arm-androideabi-linux --enable-float-approx --disable-doc --disable-extra-programs
 FLAGS="--enable-float-approx=yes --enable-custom-modes=yes --enable-assertions=yes --disable-doc --disable-extra-programs --enable-asm=yes"
+./configure $FLAGS
+cd ../
 
-case "$1" in
-    clean)
-        make clean
-        ;;
-    armeabi-v7a)
-        # Android/ARM, armeabi-v7a (ARMv7 VFP)
-        NDKABI=${NDKABI:-14}
-        TCVER=("${NDK}"/toolchains/arm-linux-androideabi-4.*)
-        NDKP=${TCVER[0]}/prebuilt/${HOST_ARCH}/bin/arm-linux-androideabi-
-        NDKF="--sysroot ${NDK}/platforms/android-${NDKABI}/arch-arm"
-        NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
-        FLAGS="$FLAGS --host=arm-androideabi-linux"
-        ./configure $FLAGS
-        make HOST_CC="gcc -m32" CFLAGS="-O2 -pipe" HOST_CFLAGS="-O2 -pipe -mtune=generic" LDFLAGS="" HOST_LDFLAGS="" TARGET_CFLAGS="${CFLAGS}" TARGET_LDFLAGS="${LDFLAGS}" TARGET_LIBS="${EXTRA_LIBS}" TARGET_SONAME="libopus.so" INSTALL_SONAME="libopus.so" CROSS="$NDKP" TARGET_FLAGS="${NDKF} ${NDKARCH}" TARGET_SYS=Linux DESTDIR="${DEST}" PREFIX=
-        ;;
-    arm64-v8a)
-        # Android/ARM, arm64-v8a (ARM64 VFP4, NEON)
-        NDKABI=${NDKABI:-21}
-        TCVER=("${NDK}"/toolchains/aarch64-linux-android-4.*)
-        NDKP=${TCVER[0]}/prebuilt/${HOST_ARCH}/bin/aarch64-linux-android-
-        NDKF="--sysroot ${NDK}/platforms/android-${NDKABI}/arch-arm64"
-        FLAGS="$FLAGS --host=aarch64-android-linux"
-        ./configure $FLAGS
-        make HOST_CC="gcc" CFLAGS="-O2 -pipe" HOST_CFLAGS="-O2 -pipe -mtune=generic" LDFLAGS="" HOST_LDFLAGS="" TARGET_CFLAGS="${CFLAGS}" TARGET_LDFLAGS="${LDFLAGS}" TARGET_LIBS="${EXTRA_LIBS}" TARGET_SONAME="libopus.so" INSTALL_SONAME="libopus.so" CROSS="$NDKP" TARGET_FLAGS="${NDKF}" TARGET_SYS=Linux DESTDIR="${DEST}" PREFIX=
-        ;;
-    x86)
-        # Android/x86, x86 (i686 SSE3)
-        NDKABI=${NDKABI:-14}
-        TCVER=("${NDK}"/toolchains/x86-4.*)
-        NDKP=${TCVER[0]}/prebuilt/${HOST_ARCH}/bin/i686-linux-android-
-        NDKF="--sysroot ${NDK}/platforms/android-${NDKABI}/arch-x86"
-        FLAGS="$FLAGS --host=i686-android-linux --disable-asm --disable-intrinsics"
-        ./configure $FLAGS
-        make HOST_CC="gcc -m32" CFLAGS="-O2 -pipe" HOST_CFLAGS="-O2 -pipe -mtune=generic" LDFLAGS="" HOST_LDFLAGS="" TARGET_CFLAGS="${CFLAGS}" TARGET_LDFLAGS="${LDFLAGS}" TARGET_LIBS="${EXTRA_LIBS}" TARGET_SONAME="libopus.so" INSTALL_SONAME="libopus.so" CROSS="$NDKP" TARGET_FLAGS="$NDKF" TARGET_SYS=Linux DESTDIR="${DEST}" PREFIX=
-        ;;
-    x86_64)
-        # Android/x86_64, x86_64
-        NDKABI=${NDKABI:-21}
-        TCVER=("${NDK}"/toolchains/x86_64-4.*)
-        NDKP=${TCVER[0]}/prebuilt/${HOST_ARCH}/bin/x86_64-linux-android-
-        NDKF="--sysroot ${NDK}/platforms/android-${NDKABI}/arch-x86_64"
-        FLAGS="$FLAGS --host=x86_64-android-linux --disable-asm --disable-intrinsics"
-        ./configure $FLAGS
-        make HOST_CC="gcc" CFLAGS="-O2 -pipe" HOST_CFLAGS="-O2 -pipe -mtune=generic" LDFLAGS="" HOST_LDFLAGS="" TARGET_CFLAGS="${CFLAGS}" TARGET_LDFLAGS="${LDFLAGS}" TARGET_LIBS="${EXTRA_LIBS}" TARGET_SONAME="libopus.so" INSTALL_SONAME="libopus.so" CROSS="$NDKP" TARGET_FLAGS="${NDKF}" TARGET_SYS=Linux DESTDIR="${DEST}" PREFIX=
-        ;;
-    *)
-        echo 'specify one of "armeabi-v7a", "arm64-v8a", "x86", "x86_64" or "clean" as first argument'
-        exit 1
-        ;;
-esac
+echo "starting"
+
+${NDK}/ndk-build NDK_PROJECT_PATH=./ NDK_APPLICATION_MK=./Application.mk APP_BUILD_SCRIPT=./Android.mk
+
+echo "done"
