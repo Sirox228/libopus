8f801a8e9da0a42a99b971adbcac25f6bf0b31e4
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-30 12:06:54 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -8,8 +8,10 @@ cd ../
 
 echo "starting"
 
-#idk why opus is so stupid that includes config.h in "" instead of <> when it is outside src folder
+#idk why opus is so stupid that includes config.h in "" instead of <> when it is outside folder with source 3 times
 cp -r opus/config.h opus/src/config.h
+cp -r opus/config.h opus/celt/config.h
+cp -r opus/config.h opus/silk/config.h
 
 ${NDK}/ndk-build NDK_PROJECT_PATH=./ NDK_APPLICATION_MK=./Application.mk APP_BUILD_SCRIPT=./Android.mk
 
