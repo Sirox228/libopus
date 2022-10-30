df1d4544844ba267b039c0278be335151455bb0d
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-30 12:04:39 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -2,7 +2,6 @@ NDK=${ANDROID_NDK_HOME}
 
 cd opus
 ./autogen.sh
-# ./configure --host=arm-androideabi-linux --enable-float-approx --disable-doc --disable-extra-programs
 FLAGS="--enable-float-approx=yes --enable-custom-modes=yes --enable-assertions=yes --disable-doc --disable-extra-programs --enable-asm=yes"
 ./configure $FLAGS
 cd ../
