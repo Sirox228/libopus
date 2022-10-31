21ada01f32ab774c6a8b454915f940ec5827e487
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-31 21:37:03 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -8,8 +8,8 @@ cd ../
 
 echo "starting"
 
-${NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-as --help
-${NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-as -o opus/celt/arm/celt_pitch_xcorr_arm.o opus/celt/arm/celt_pitch_xcorr_arm.s
+./aarch64-linux-android-as --help
+./aarch64-linux-android-as -o opus/celt/armopt.o opus/celt/armopt.s
 
 ${NDK}/ndk-build NDK_PROJECT_PATH=./ NDK_APPLICATION_MK=./Application.mk APP_BUILD_SCRIPT=./Android.mk
 
