6b08c83634aea8bc0a8646f75a5b93055b93b907
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-31 21:22:30 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -8,7 +8,7 @@ cd ../
 
 echo "starting"
 
-./aarch64-linux-android-as -c opus/celt/arm/armopts.s -o opus/celt/arm/armopts.o
+./aarch64-linux-android-as -c opus/celt/arm/celt_pitch_xcorr_arm.s -o opus/celt/arm/celt_pitch_xcorr_arm.o
 
 ${NDK}/ndk-build NDK_PROJECT_PATH=./ NDK_APPLICATION_MK=./Application.mk APP_BUILD_SCRIPT=./Android.mk
 
